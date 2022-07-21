69646b9463a43a05df9c47b7c3c7df42669821b3
Slava <mr.baranok.slava@gmail.com>
2022-07-21 23:53:43 +0300

    add skeleton in about me

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import style from "./AboutMe.module.css"
+import styleContainer from "../../common/styles/Container.module.css"
 
 
 const AboutMe = () => {
     return (
         <div className={style.about}>
-            <div className={style.text}></div>
+            <div className={styleContainer.wrapper}>
+            <div className={style.text}>
+                <p>Hi there</p>
+                <h1>I'm Slava Baranok</h1>
+                <p>I'm frontend developer</p>
+            </div>
             <div className={style.photo}></div>
+            </div>
         </div>
     );
 };
