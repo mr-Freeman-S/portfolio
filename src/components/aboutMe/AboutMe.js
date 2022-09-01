8c064ef75b768151f0450513f0b08f4c1d5ca444
Slava <mr.baranok.slava@gmail.com>
2022-09-01 23:55:42 +0300

    add to variables font and color

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -4,17 +4,18 @@ import styleContainer from "../../common/styles/Container.module.scss"
 import Header from "../Header/Header";
 
 
+
 const AboutMe = () => {
     return (
         <div className={style.about}>
             <Header/>
             <div className={`${styleContainer.wrapper} ${style.container}`}>
-            <div className={style.text}>
-                <p>Hi there</p>
-                <h1>I'm Slava Baranok</h1>
-                <p>I'm frontend developer</p>
-            </div>
-            <div className={style.photo}></div>
+                <div className={style.text}>
+                    <h4>Hi there</h4>
+                    <h1>I'm Slava Baranok</h1>
+                    <h4>I'm frontend developer</h4>
+                </div>
+                <div className={style.photo}></div>
             </div>
         </div>
     );
