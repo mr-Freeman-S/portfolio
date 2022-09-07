5229142bd66414731f0c0b73ac2bfb4392086898
Slava <mr.baranok.slava@gmail.com>
2022-09-07 23:37:11 +0300

    refactoring skills page

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -5,15 +5,13 @@ import Skill from "./skill/Skill";
 import styleTitle from '../../common/styles/Title.module.scss'
 
 const Skills = () => {
+    const mySkills = ['HTML|CSS', 'JavaScript| ES6', 'ReactJS', 'Redux', 'Redux-thunk', 'MaterialUI', 'Tailwind', 'Bootstrap']
     return (
         <div className={style.wrapper}>
             <div className={`${styleContainer.wrapper} ${style.container}`}>
                 <h2 className={styleTitle.title}>Skills</h2>
                 <div className={style.skills}>
-                    <Skill title={"js"} description={'asdas dasdasdasd'}/>
-                    <Skill title={"css/html"}
-                           description={'asdas dasdddddddddd dddddddddd ddddddddd dddddddddddd dddddddddddddddddasd asddddddddddddddddsad asdasd'}/>
-                    <Skill title={"react"} description={'asdas dasdas asdasd asdas das das dsadasd asdasdasd'}/>
+                    {mySkills.map((el,key) => <Skill key={key} title={el} description={'Hello world'} />)}
                 </div>
             </div>
         </div>
