8b0fa613d3d270970e4d93e33da0ab078f9992a8
Slava <mr.baranok.slava@gmail.com>
2022-09-11 22:18:49 +0300

    add projects

--- src/components/myProjects/MyProjects.jsx
+++ src/components/myProjects/MyProjects.jsx
@@ -3,19 +3,31 @@ import style from './MyProjects.module.scss'
 import styleContainer from "../../common/styles/Container.module.scss";
 import Project from "./project/Project";
 import BlockTitle from "../../common/components/BlockTitle/BlockTitle";
+import socialNetwork from './../../assets/images/socialNetwork.jpeg'
+import todoList from './../../assets/images/todoList.jpg'
+import learningCards from './../../assets/images/learningCards.jpeg'
+import pizzaOrder from './../../assets/images/pizzaOrder.jpg'
 
 const MyProjects = () => {
-    const myProjects = [{title:'Social network', img:'https://medium.com/hackernoon/social-network-development-types-challenges-technologies-costs-1e185da3b2a9'}]
+    const myProjects = [
+        {title: 'Social network', img: socialNetwork, path: '#', description: 'Stack:Hello world'},
+        {title: 'Todo list', img: todoList, path: '#', description: 'Stack:Masdasdas asdas'},
+        {title: 'Learning cards', img: learningCards, path: '#', description: 'Stack:   asdas dasd asd as '},
+        {
+            title: 'Pizza order', img: pizzaOrder, path: '#', description: 'Stack:asdas dsa das das  dsa ds'
+        }
+    ]
 
     return (
         <div className={style.wrapper}>
             <div className={`${styleContainer.wrapper} ${style.container}`}>
                 <BlockTitle preTitle='Showcasing some of my work' title='Projects'/>
                 <div className={style.projectWrapper}>
-                    <Project img={myProjects[0].img} projectName={'social network'} shortDescription={'bla bal asdasdas fdasfas afasf aaaaaaa'}/>
-                    <Project img={'img1'} projectName={'social network'} shortDescription={'bla bal asdasdas fdasfas afasf aaaaaaa'}/>
-                    <Project img={'img1'} projectName={'cards'} shortDescription={'blaasf aadsad sadas dsa das dsa dsa dsad as das dsa das dsa dsa das dsad as dsa dasdas das d asd sadasdas dsadasdasaaaaa'}/>
-                    <Project img={'img1'} projectName={'cards'} shortDescription={'blaasf aaaaaaa'}/>
+                    {myProjects.map((el,key) => (
+                        <Project img={el.img} title={el.title}
+                                 description={el.description} path={el.path}/>))}
+
+
                 </div>
             </div>
         </div>
--- src/components/myProjects/project/Project.jsx
+++ src/components/myProjects/project/Project.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import style from './Project.module.scss'
 import {Link} from "react-router-dom";
 
+
 const Project = (props) => {
     return (
         <div className={style.wrapper}>
             <div className={style.imgContainer}>
-                <Link className={style.link} to={{ pathname: "#" }} target="_blank" >View</Link>
+                <Link style={{
+                    backgroundImage: `url(${props.img})`,
+                    backgroundSize: 'cover',
+                    height: '100%',
+                    width: '100%'
+                }} className={style.link} to={{pathname: props.path}} target="_blank"></Link>
             </div>
-            <h2 className={style.title}>{props.projectName}</h2>
-            <span className={style.description}>{props.shortDescription}</span>
+            <h2 className={style.title}>{props.title}</h2>
+            <span className={style.description}>{props.description}</span>
         </div>
     );
 };
