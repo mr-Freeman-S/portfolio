6d295efa2aec6636992dd69bf2104ab00d7dd46a
Slava <mr.baranok.slava@gmail.com>
2022-10-06 21:20:37 +0300

    styled contact info

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,8 +2,16 @@ import React from 'react';
 import style from './ContactForm.module.scss'
 import styleContainer from '../../common/styles/Container.module.scss'
 import BlockTitle from "../../common/components/BlockTitle/BlockTitle";
+import signature from "./../../assets/logo/signature.svg"
+import location from "./../../assets/logo/map-location.svg"
+import phone from "./../../assets/logo/phone.svg"
+import email from "./../../assets/logo/paper-plane.svg"
+
+
 
 const ContactForm = () => {
+
+
     return (
         <div className={style.wrapper}>
             <div className={`${styleContainer.wrapper} ${style.formWrapper}`}>
@@ -34,28 +42,23 @@ const ContactForm = () => {
                                 along, Feel free to contact me!</p>
                             <ul>
                                 <li>
-                                    <div className={style.media}><span className={style.iconInfo}></span>
-                                        <div><h6></h6><span></span></div>
+                                    <div className={style.media}><span className={style.iconInfo}><img src={signature} alt="name"/></span>
+                                        <div className={style.mediaBody}><h6 className={style.infoType}>Name</h6><span className={style.infoValue}>Vuachaslau Baranok</span></div>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className={style.media}><span className={style.iconInfo}></span>
-                                        <div><h6></h6><span></span></div>
+                                    <div className={style.media}><span className={style.iconInfo}><img src={location} alt="location"/></span>
+                                        <div className={style.mediaBody}><h6 className={style.infoType}>Location</h6><span className={style.infoValue}>220000 Belarus, Minsk</span></div>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className={style.media}><span className={style.iconInfo}></span>
-                                        <div><h6></h6><span></span></div>
+                                    <div className={style.media}><span className={style.iconInfo}><img src={phone} alt=""/></span>
+                                        <div className={style.mediaBody}><h6 className={style.infoType}>Call Me</h6><span className={style.infoValue}>+375339018435</span></div>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className={style.media}><span className={style.iconInfo}></span>
-                                        <div><h6></h6><span></span></div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className={style.media}><span className={style.iconInfo}></span>
-                                        <div><h6></h6><span></span></div>
+                                    <div className={style.media}><span className={style.iconInfo}><img src={email} alt=""/></span>
+                                        <div className={style.mediaBody}><h6 className={style.infoType}>Email Me</h6><span className={style.infoValue}>mr.baranok.slava@gmail.com</span></div>
                                     </div>
                                 </li>
                             </ul>
